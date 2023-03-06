36566075fcb39d34592680f60e544ffda57cd63d
Luis Isaac <luiscubiche@gmail.com>
2023-03-06 13:17:04 -0500

    backen push

--- Sever-Project3/routes/games.js
+++ Sever-Project3/routes/games.js
@@ -3,19 +3,21 @@ var router = express.Router();
 
 // Models to populate 
 
-router.get('/', (req, res, next) => {
-    Post.find()
-    /*  
-    .populate('contributor')
-      .populate('country')
-      */
-      .sort({createdAt: -1})
-      .then((foundGame) => {
-          res.json(foundGame)
-      })
-      .catch((err) => {
-          console.log(err)
-      })
-  });
+// router.get('/', (req, res, next) => {
+//     Post.find()
+//     /*  
+//     .populate('contributor')
+//       .populate('country')
+//       */
+//       .sort({createdAt: -1})
+//       .then((foundGame) => {
+//           res.json(foundGame)
+//       })
+//       .catch((err) => {
+//           console.log(err)
+//       })
+//   });
+
+
 
   module.exports = router;
